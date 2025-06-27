746530fe44f65532f2d969d7e6731919af50cf20
LPF-24 <mwlosev@gmail.com>
2025-06-27 13:54:52 +0300

    feat(notification-service): - logging is now done using slf4j.Logger , not using System.out.println - code readability and purity has been improved

--- src/main/java/com/example/notification_service/service/NotificationListener.java
+++ src/main/java/com/example/notification_service/service/NotificationListener.java
@@ -1,14 +1,17 @@
 package com.example.notification_service.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationListener {
+    private final Logger logger = LoggerFactory.getLogger(NotificationListener.class);
 
     @KafkaListener(topics = "habit-events", groupId = "notification-group")
     public void listen(String message) {
-        System.out.println("Получено событие из Kafka: " + message);
+        logger.info("Received event from Kafka: {}", message);
         // в реальной системе тут была бы отправка email/уведомления
     }
 }
